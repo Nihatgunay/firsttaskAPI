964b8ca6aa082fa647a7cdc2a1bec61bda7a5ce8
NIhat <naliyev515@gmail.com>
2024-09-11 02:25:59 +0400

    added

--- FirstWebApi/FirstWebApi/obj/Debug/net8.0/FirstWebApi.AssemblyInfo.cs
+++ FirstWebApi/FirstWebApi/obj/Debug/net8.0/FirstWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e76d3df274b690f8ce4b3d8777f8d67b1a3e50")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
